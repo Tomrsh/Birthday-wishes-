88232c14aab47173f5e75f67548580b2202d2963
T2 Official <157962573+Tomrsh@users.noreply.github.com>
2025-03-23 13:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ let images = [
   "https://github.com/Tomrsh/Birthday-wishes-/blob/main/IMG-20250320-WA0009.jpg",
   "https://github.com/Tomrsh/Birthday-wishes-/blob/main/IMG-20250224-WA0032.jpg",
   "https://github.com/Tomrsh/Birthday-wishes-/blob/main/IMG-20250210-WA0030.jpg",
-  "/IMG_20250322_165118.jpg"
+  "https://github.com/Tomrsh/Birthday-wishes-/blob/main/IMG-20250320-WA0012.jpg"
 ];
 
 let imgIndex = 0;
